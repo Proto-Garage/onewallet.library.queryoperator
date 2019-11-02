e24d80cf4f864c0b875d11d37aa2c6db2099d222
Roger Madjos <roger.madjos@gmail.com>
2019-11-02 20:34:55 +0800

    build: add postinstall script

--- build/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as convertToMongooseQuery } from './lib/convert-to-mongoose-query';
-export { default as convertToSequelizeQuery } from './lib/convert-to-sequelize-query';
-export * from './types';
-//# sourceMappingURL=index.d.ts.map
--- build/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var convert_to_mongoose_query_1 = require("./lib/convert-to-mongoose-query");
-exports.convertToMongooseQuery = convert_to_mongoose_query_1.default;
-var convert_to_sequelize_query_1 = require("./lib/convert-to-sequelize-query");
-exports.convertToSequelizeQuery = convert_to_sequelize_query_1.default;
-//# sourceMappingURL=index.js.map
